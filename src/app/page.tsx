e6188030bf5b390bdadb029d0d5cbe9f6041300d
Guilherme Poletti <gui.poletti@gmail.com>
2023-06-09 11:04:43 -0300

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play} from 'lucide-react'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Maximize2, Volume } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -6,11 +6,11 @@ export default function Home() {
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
-          
+
           <div className="flex items-center gap-2">
-            <div className="w-2 h-2 bg-red-500 rounded-full"/>
-            <div className="w-2 h-2 bg-yellow-500 rounded-full"/>
-            <div className="w-2 h-2 bg-green-500 rounded-full"/>
+            <div className="w-2 h-2 bg-red-500 rounded-full" />
+            <div className="w-2 h-2 bg-yellow-500 rounded-full" />
+            <div className="w-2 h-2 bg-green-500 rounded-full" />
           </div>
 
           <nav className="space-y-5 mt-10">
@@ -36,7 +36,7 @@ export default function Home() {
           </nav>
 
         </aside>
-        
+
         <main className="flex-1 p-6">
           <div className="flex items-center gap-3">
             <button className="rounded-full bg-black/40 p-2">
@@ -51,7 +51,7 @@ export default function Home() {
 
           <div className="grid grid-cols-3 gap-4 mt-4">
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="" width={104} height={104} alt="Capa do CD"/>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do CD" />
               <strong>Titulo Cd</strong>
 
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible ">
@@ -59,7 +59,7 @@ export default function Home() {
               </button>
             </a>
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="" width={104} height={104} alt="Capa do CD"/>
+              <Image src="/acustico.jpg" width={104} height={104} alt="Capa do CD" />
               <strong>Titulo Cd</strong>
 
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible ">
@@ -67,7 +67,7 @@ export default function Home() {
               </button>
             </a>
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="" width={104} height={104} alt="Capa do CD"/>
+              <Image src="/asf.jpg" width={104} height={104} alt="Capa do CD" />
               <strong>Titulo Cd</strong>
 
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible ">
@@ -75,7 +75,7 @@ export default function Home() {
               </button>
             </a>
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="" width={104} height={104} alt="Capa do CD"/>
+              <Image src="/pcpl.jpg" width={104} height={104} alt="Capa do CD" />
               <strong>Titulo Cd</strong>
 
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible ">
@@ -83,7 +83,7 @@ export default function Home() {
               </button>
             </a>
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="" width={104} height={104} alt="Capa do CD"/>
+              <Image src="/rrr.jpg" width={104} height={104} alt="Capa do CD" />
               <strong>Titulo Cd</strong>
 
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible ">
@@ -91,7 +91,7 @@ export default function Home() {
               </button>
             </a>
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="" width={104} height={104} alt="Capa do CD"/>
+              <Image src="/tamo-ai-na-atividade.jpg" width={104} height={104} alt="Capa do CD" />
               <strong>Titulo Cd</strong>
 
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible ">
@@ -100,13 +100,82 @@ export default function Home() {
             </a>
           </div>
 
+          <h2 className="font-semibold text-2xl mt-10">Made for Guilherme Poletti</h2>
+
+          <div className="grid grid-cols-8 gap-5 mt-4">
+            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/rrr.jpg" className="w-full" width={120} height={120} alt="Capa PlayList" />
+              <strong className='font-semibold'>Daily mix</strong>
+              <span className="text-sm text-zinc-400">Charlie Brown Jr, Forfun, Fresno, NxZero and more</span>
+            </a>
+            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/rrr.jpg" className="w-full" width={120} height={120} alt="Capa PlayList" />
+              <strong className='font-semibold'>Daily mix</strong>
+              <span className="text-sm text-zinc-400">Charlie Brown Jr, Forfun, Fresno, NxZero and more</span>
+            </a>
+            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/rrr.jpg" className="w-full" width={120} height={120} alt="Capa PlayList" />
+              <strong className='font-semibold'>Daily mix</strong>
+              <span className="text-sm text-zinc-400">Charlie Brown Jr, Forfun, Fresno, NxZero and more</span>
+            </a>
+            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/rrr.jpg" className="w-full" width={120} height={120} alt="Capa PlayList" />
+              <strong className='font-semibold'>Daily mix</strong>
+              <span className="text-sm text-zinc-400">Charlie Brown Jr, Forfun, Fresno, NxZero and more</span>
+            </a>
+            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/rrr.jpg" className="w-full" width={120} height={120} alt="Capa PlayList" />
+              <strong className='font-semibold'>Daily mix</strong>
+              <span className="text-sm text-zinc-400">Charlie Brown Jr, Forfun, Fresno, NxZero and more</span>
+            </a>
+          </div>
+
         </main>
-      
       </div>
-      
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer  
-      </footer>  
-    </div> 
+
+      <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image src="/tamo-ai-na-atividade.jpg" width={64} height={64} alt="Capa do CD" />
+          <div className='flex flex-col'>
+            <strong className='font-normal'>Tamo ai na atividade</strong>
+            <span className='text-xs text-zinc-400'>Charlie Brown Jr.</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-6">
+            <Shuffle size={20} className="text-zinc-200" />
+            <SkipBack size={20} className="text-zinc-200" />
+
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white-400 text-black">
+              <Play />
+            </button>
+
+            <SkipForward size={20} className="text-zinc-200" />
+            <Repeat size={20} className="text-zinc-200" />
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-400">2:14</span>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Mic2 size={20} />
+          <LayoutList size={20} />
+          <Laptop2 size={20} />
+          <div className="flex items-center gap-2">
+            <Volume size={20} />
+            <div className="h-1 rounded-full w-24 bg-zinc-600">
+              <div className="bg-zinc-200 w-1 h-1 rounded-full"></div>
+            </div>
+          </div>
+          <Maximize2 size={20} />
+        </div>
+      </footer>
+    </div>
   )
 }
